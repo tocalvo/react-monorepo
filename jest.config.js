387e75591afac398186fd58e666a6f46535a7723
Tom Calvo <calvo.tom@gmail.com>
2020-11-11 07:28:40 +0100

    more demos, shared ui component, test building

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  projects: ['<rootDir>/apps/demo-next'],
+  projects: [
+    '<rootDir>/apps/demo-next',
+    '<rootDir>/apps/demo-next-2',
+    '<rootDir>/apps/demo-react',
+    '<rootDir>/libs/ui',
+    '<rootDir>/apps/demo-react-2',
+  ],
 };
